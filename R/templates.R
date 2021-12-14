5c7dee41559801068162e98de770e62b6c1f4f72
Jonathan Zadra <jzadra@integral-corp.com>
2021-12-14 15:24:18 -0700

    add packagedown. Add templates

---
+++ R/templates.R
@@ -0,0 +1,40 @@
+#' Start a new R script with header
+#' @description Creates a new R script with header
+#' @param filepath File to be created. If path is not included, it will be created in the current working directory.
+#' @examples
+#' \dontrun{
+#' ic_new_r_file("myfile.R")
+#' }
+#' @export
+ic_new_r_file <- function(filepath) {
+  use_template("r_header/r file with header.R", save_as = filepath, open = TRUE, package = "integral")
+}
+
+
+
+#' Start a new RMarkdown document with Integral header
+#' @description Drafts a new RMarkdown document with Integral header
+#' @param filepath File to be created. If path isn't provided, working directory is used.
+#' @examples
+#' \dontrun{
+#' ig_new_rmd_file("myrmd.Rmd")
+#' }
+#' @export
+ig_new_rmd_file <- function(file) {
+  if(dirname(file) == ".") {
+    message("Directory not provided. Using working directory.")
+    file <- file.path(getwd(), file)
+  }
+
+  if(stringr::str_to_lower(tools::file_ext(file)) != "rmd") {
+    cli::cli_alert_danger("File extension should be .Rmd")
+    return(NULL)
+  }
+
+  rmarkdown::draft(file = file,
+                   template = "r_markdown_with_header",
+                   package = "integral",
+                   edit = F)
+
+  file.edit(file.path(tools::file_path_sans_ext(file), basename(file)))
+}
