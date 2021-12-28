a86494809979ba488aeeed92ac8cda0a9bab336a
Jonathan Zadra <jzadra@integral-corp.com>
2021-12-28 12:04:58 -0700

    add cheatsheets shortcut

---
+++ R/shortcuts.R
@@ -0,0 +1,17 @@
+#' Open cheatsheet
+#' @description
+#' `r lifecycle::badge('experimental')`
+#' Shortcut to open a cheatsheet
+#' @param sheet Name of the cheatsheet, if known. If NULL (default) a list is presented.
+#' @export
+cheat <- function(sheet = NULL) {
+
+  if(!UseMethod(hasArg(sheet))) {
+    cli::cli_alert_info("Please select cheetsheet:")
+    sheet <- utils::select.list(choices = dir(system.file("extdata/cheatsheets", package = "integral")))
+  }
+
+  fpath <- system.file("extdata/cheatsheets", sheet, package = "integral")
+  system2("open", fpath)
+
+}
