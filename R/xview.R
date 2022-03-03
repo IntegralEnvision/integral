cb7678229a44b490daca5a9eecf611615299e4e0
Jonathan Zadra <jzadra@gmail.com>
2022-03-03 15:27:01 -0700

    added xview2 for testing. name will change in the future.

--- R/xview.R
+++ R/xview.R
@@ -50,3 +50,69 @@ xview <- function (.data, add_rownames = TRUE) {
     system2("open", preferredFilePath)
   }
 }
+
+
+xview2 <- function (.data, add_rownames = TRUE, auto_conditional = TRUE) {
+
+
+  if(length(dim(.data))>2){
+    stop('Too many dimensions')
+  }
+  if(is.null(dim(.data))){
+    .data = as.data.frame(.data)
+  }
+
+  if(add_rownames & tibble::has_rownames(.data)) .data <- .data %>% tibble::rownames_to_column() else
+    .data <- .data %>% mutate(rownum = row_number(), .before = 1)
+
+
+
+  if(add_rownames & tibble::has_rownames(data)) .data <- .data %>% tibble::rownames_to_column()
+
+  wb <- createWorkbook()
+
+  addWorksheet(wb, "xview")
+
+
+  writeData(wb, "xview", .data)
+
+  addFilter(wb, 1, rows = 1, cols = 1:ncol(.data))
+
+  freezePane(wb, 1, firstRow = T, firstCol = T)
+
+  if(auto_conditional) {
+    colclasses <- lapply(.data, class) %>%
+      as_tibble %>%
+      slice(1) %>%
+      pivot_longer(everything()) %>%
+      mutate(colnum = row_number()) %>%
+      filter(name != "rowname") %>%
+      filter(name != "rownum")
+
+    colclasses %>%
+      rowwise() %>%
+      group_split() %>%
+      walk(function(x) {
+        switch(x$value,
+               numeric = conditionalFormatting(wb, 1, cols = x$colnum, rows = 2:nrow(.data) + 1,
+                                               type = "colourScale",
+                                               style = c("cyan", "white", "darkorchid1")),
+
+               integer = conditionalFormatting(wb, 1, cols = x$colnum, rows = 2:nrow(.data) + 1,
+                                               type = "colourScale",
+                                               style = c("cyan", "white", "darkorchid1")),
+
+               ordered = conditionalFormatting(wb, 1, cols = x$colnum, rows = 2:nrow(.data) + 1,
+                                               type = "colourScale",
+                                               style = c("#edf8b1", "#7fcdbb", "#2c7fb8")),
+        )
+      })
+  }
+
+  openxl_temp <- temp_xlsx()
+  saveWorkbook(wb, openxl_temp)
+
+  openXL(openxl_temp)
+
+
+}
