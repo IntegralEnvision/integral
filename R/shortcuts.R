3e74c439107b736a33ee7ead1513efafed583c67
Jonathan Zadra <jzadra@gmail.com>
2022-04-28 13:02:24 -0600

    added handling of no matches when sheet argument is given.

--- R/shortcuts.R
+++ R/shortcuts.R
@@ -14,11 +14,15 @@ cheat <- function(sheet = NULL) {
     sheet <- utils::select.list(choices = files)
    } else if(any(stringr::str_detect(files, sheet))) { #Matches
     matches <- files[stringr::str_detect(files, sheet)]
-    if(length(matches) == 1) sheet <- matches else {
+    if(length(matches) == 1) sheet <- matches  else {
       cli::cli_alert_info("Multiple matching cheatsheets, select one:")
       sheet <- utils::select.list(choices = matches)
-    }
-  }
+      }
+   } else {
+        cli::cli_alert_warning("No matching cheatsheets. Select from the list below:")
+       sheet <- utils::select.list(choices = files)
+     }
+
 
   sheet <- paste0(sheet, ".pdf")
 
