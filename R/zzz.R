9a7526aac659119730d948fb95367442d97d67b6
Jonathan Zadra <jzadra@gmail.com>
2022-05-02 11:37:31 -0600

    temporarily disabling the auto-update check .onLoad code to avoid issues do to repo name change.

--- R/zzz.R
+++ R/zzz.R
@@ -1,16 +1,18 @@
-.onLoad <- function(...) {
-  try(check_ic_up_to_date())
-}
+#This is currently commented out because it refers to the non-private integral
 
-check_ic_up_to_date <- function() {
-  rem <- structure(remotes:::package2remote("integral"), class = "github_remote")
-  if(is.na(rem[["sha"]])) return()
-  rem <- remotes:::remote_sha(rem)
-
-  loc <- remotes:::local_sha("integral")
-
-  if(loc != rem) {
-    cli::cli_alert_info(cli::bg_red(cli::col_white(cli::style_bold("A newer version of \`integral\` is available.  Run \`ic_update()\` to update."))))
-  }
-
-}
+# .onLoad <- function(...) {
+#   try(check_ic_up_to_date())
+# }
+#
+# check_ic_up_to_date <- function() {
+#   rem <- structure(remotes:::package2remote("integral"), class = "github_remote")
+#   if(is.na(rem[["sha"]])) return()
+#   rem <- remotes:::remote_sha(rem)
+#
+#   loc <- remotes:::local_sha("integral")
+#
+#   if(loc != rem) {
+#     cli::cli_alert_info(cli::bg_red(cli::col_white(cli::style_bold("A newer version of \`integral\` is available.  Run \`ic_update()\` to update."))))
+#   }
+#
+# }
