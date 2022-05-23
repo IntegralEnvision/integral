b8eb56a29a69b2a515f9f4ce526f27abaafe9c66
Jonathan Zadra <jzadra@gmail.com>
2022-05-23 17:35:35 -0600

    added prefixes, changed a few variable names.  Tested on test file; working.

--- R/qa.R
+++ R/qa.R
@@ -10,22 +10,20 @@ qa <- function(filepath) {
 
   filepath <- path_real(filepath)
 
-  qa_file <- qa_file(filepath)
+  qafile <- qa_file(filepath)
 
-  qa_wb(filepath, qa_file)
+  qawb <- qa_wb(filepath, qafile)
 
   parsed_qa <- qa_parse(filepath)
 
-  qawb <- qa_wb_load(filepath)
-
-  qa_update_sheet(qawb, parsed_qa, filepath)
+  qa_update_sheet(qawb, parsed_qa, filepath, qafile)
 
   }
 
 
 qa_parse <- function(filepath) {
 
-  filetype <- tools::file_ext(filepath) %>% str_to_lower()
+  filetype <- tools::file_ext(filepath) %>% stringr::str_to_lower()
 
   script_qa <- fs::path_expand(filepath)
 
@@ -35,13 +33,13 @@ qa_parse <- function(filepath) {
 
   #Check for QA tags and QA tags without IDs
   has_qa_tags <- any(all_code %>%
-                       mutate(has_qa = stringr::str_detect(code, "QA:")) %>%
-                       pull(has_qa))
+                       dplyr::mutate(has_qa = stringr::str_detect(code, "QA:")) %>%
+                       dplyr::pull(has_qa))
   missing_qa_ids <- any(all_code %>%
-                          mutate(has_id = stringr::str_detect(code, "(QA: )(\\d+)(.*)")) %>%
-                          mutate(missing_id = stringr::str_detect(code, "#\\s?QA:\\s(?=\\D)")) %>%
-                          mutate(doublecheck = !has_id & missing_id) %>%
-                          pull(doublecheck))
+                          dplyr::mutate(has_id = stringr::str_detect(code, "(QA: )(\\d+)(.*)")) %>%
+                          dplyr::mutate(missing_id = stringr::str_detect(code, "#\\s?QA:\\s(?=\\D)")) %>%
+                          dplyr::mutate(doublecheck = !has_id & missing_id) %>%
+                          dplyr::pull(doublecheck))
 
   if(!has_qa_tags) stop("The file does not contain any QA tags.")
 
@@ -50,98 +48,99 @@ qa_parse <- function(filepath) {
     if(add_ids) {
 
       #Temporary backup solution
-      if(!fs::dir_exists(path(path_dir(filepath), "backup"))) fs::dir_create(path(path_dir(filepath), "backup"))
-      fs::file_copy(filepath, path(path_dir(filepath), "backup", paste0(fs::path_sanitize(lubridate::now()), " - ", path_file(filepath))))
+      if(!fs::dir_exists(fs::path(fs::path_dir(filepath), "backup"))) fs::dir_create(fs::path(fs::path_dir(filepath), "backup"))
+      fs::file_copy(filepath, fs::path(fs::path_dir(filepath), "backup", paste0(fs::path_sanitize(lubridate::now()), " - ", fs::path_file(filepath))))
       #/Temporary backup solution
 
       all_code <- all_code %>%
-        mutate(has_qa = stringr::str_detect(code, "QA:")) %>%
+        dplyr::mutate(has_qa = stringr::str_detect(code, "QA:")) %>%
         mutate(has_id = stringr::str_detect(code, "(QA: )(\\d+)(.*)")) %>%
         mutate(missing_id = stringr::str_detect(code, "#\\s?QA:\\s(?=\\D)")) %>%
         rowwise() %>%
-        mutate(code = if_else(has_qa & !has_id & missing_id, str_replace(code, "#\\s?QA:\\s(?=\\D)", paste0("# QA: ", round(runif(1, 1, 10000)), " | ")), code)) %>%
-        ungroup() %>%
-        select(-c(has_qa, has_id, missing_id))
+        dplyr::mutate(code = dplyr::if_else(has_qa & !has_id & missing_id, stringr::str_replace(code, "#\\s?QA:\\s(?=\\D)", paste0("# QA: ", round(runif(1, 1, 10000)), " | ")), code)) %>%
+        dplyr::ungroup() %>%
+        dplyr::select(-c(has_qa, has_id, missing_id))
 
-      write_lines(all_code$code, filepath)
+      readr::write_lines(all_code$code, filepath)
       cli::cli_alert_success("Your script has been modified to include QA ID tags. If it is currently open, you will be prompted to reload it. A backup has been created in the 'backups' subdirectory.")
     }
   }
 
   if(filetype == "rmd") {
     all_code <- all_code %>% #Add flag to indicate whether the lines are in a text chunk. We use this later when parsing section headers
-      mutate(chunk_deliniator = str_detect(code, "^```")) %>%
-      mutate(chunk_num = cumsum(chunk_deliniator)) %>%
-      mutate(is_text_chunk = chunk_num %% 2 == 0 & !chunk_deliniator) %>%
-      select(-c(chunk_deliniator, chunk_num))
-  } else if(filetype == "r") all_code <- all_code %>% add_column(is_text_chunk = F)
+      dplyr::mutate(chunk_deliniator = stringr::str_detect(code, "^```")) %>%
+      dplyr::mutate(chunk_num = cumsum(chunk_deliniator)) %>%
+      dplyr::mutate(is_text_chunk = chunk_num %% 2 == 0 & !chunk_deliniator) %>%
+      dplyr::select(-c(chunk_deliniator, chunk_num))
+  } else if(filetype == "r") all_code <- all_code %>% tibble::add_column(is_text_chunk = F)
 
   headers <- all_code %>%
-    dplyr::mutate(is_code_header = str_detect(code, "(#+)[^\\t]([a-zA-Z0-9\\(\\)&\\s]*)(?=-+)")) %>%
-    dplyr::mutate(is_text_header = is_text_chunk & str_detect(code, "(#+)\\s(.*?)")) %>%
-    mutate(code = str_remove_all(code, "-{2,}")) %>%
-    filter(is_code_header | is_text_header) %>%
-    mutate(section = str_match(code, "(#+)\\s(.*)")) %>%
-    mutate(section = as_tibble(section)) %>%
-    unnest_wider(section, names_sep = "_") %>%
-    rename(full_string = section_V1,
+    dplyr::mutate(is_code_header = stringr::str_detect(code, "(#+)[^\\t]([a-zA-Z0-9\\(\\)&\\s]*)(?=-+)")) %>%
+    dplyr::mutate(is_text_header = is_text_chunk & stringr::str_detect(code, "(#+)\\s(.*?)")) %>%
+    dplyr::mutate(code = stringr::str_remove_all(code, "-{2,}")) %>%
+    dplyr::filter(is_code_header | is_text_header) %>%
+    dplyr::mutate(section = stringr::str_match(code, "(#+)\\s(.*)")) %>%
+    dplyr::mutate(section = dplyr::as_tibble(section)) %>%
+    tidyr::unnest_wider(section, names_sep = "_") %>%
+    dplyr::rename(full_string = section_V1,
            hash = section_V2,
            title = section_V3) %>%
-    mutate(full_string = str_squish(full_string),
-           title = str_squish(title)) %>%
-    mutate(section_level = str_count(hash, "#")) %>%
-    select(-c(is_text_header, is_code_header, full_string, hash, is_text_chunk))
+    dplyr::mutate(full_string = stringr::str_squish(full_string),
+           title = stringr::str_squish(title)) %>%
+    dplyr::mutate(section_level = stringr::str_count(hash, "#")) %>%
+    dplyr::select(-c(is_text_header, is_code_header, full_string, hash, is_text_chunk))
 
-  section_depth <- headers %>% summarize(section_depth = max(section_level)) %>% pull(section_depth)
+  section_depth <- headers %>% dplyr::summarize(section_depth = max(section_level)) %>% dplyr::pull(section_depth)
 
   wh <- headers %>% #wide headers
-    select(-file_full) %>%
-    pivot_wider(names_from = section_level, values_from = title, names_prefix = "level_")
+    dplyr::select(-file_full) %>%
+    tidyr::pivot_wider(names_from = section_level, values_from = title, names_prefix = "level_")
 
   for(i in seq(section_depth)) {
     wh <- wh %>%
       fill(paste0("level_", i), .direction = "down") %>%
-      group_by_at(paste0("level_", i), .add = T)
+      dplyr::group_by_at(paste0("level_", i), .add = T)
   }
 
-  wh <- wh %>% ungroup()
+  wh <- wh %>% dplyr::ungroup()
 
   qa_lines <- all_code %>%
-    select(-c(is_text_chunk, file_full)) %>%
-    filter(stringr::str_detect(code, "QA:"))
+    dplyr::select(-c(is_text_chunk, file_full)) %>%
+    dplyr::filter(stringr::str_detect(code, "QA:"))
 
   qa_lines <- qa_lines %>%
-    mutate(qa_comment = str_match(code, "(QA: )(\\d+)(.*)")) %>%
-    mutate(qa_comment = as_tibble(qa_comment)) %>%
-    unnest_wider(qa_comment, names_sep = "_") %>%
-    rename(qa_id = qa_comment_V3,
+    dplyr::mutate(qa_comment = stringr::str_match(code, "(QA: )(\\d+)(.*)")) %>%
+    dplyr::mutate(qa_comment = dplyr::as_tibble(qa_comment)) %>%
+    tidyr::unnest_wider(qa_comment, names_sep = "_") %>%
+    dplyr::rename(qa_id = qa_comment_V3,
            comment = qa_comment_V4) %>%
-    select(-c(qa_comment_V2, qa_comment_V1, code)) %>%
-    mutate(comment = str_remove(comment, " \\| "))
+    dplyr::select(-c(qa_comment_V2, qa_comment_V1, code)) %>%
+    dplyr::mutate(comment = stringr::str_remove(comment, " \\| ")) %>%
+    dplyr::mutate(qa_id = as.numeric(qa_id))
 
   #Determine the location of QA lines within sections
   m_ind <- qa_lines %>%
-    select(line) %>%
-    add_column(table = "qa") %>%
-    bind_rows(wh %>%
-                select(line) %>%
-                add_column(table = "section")) %>%
-    arrange(line) %>%
-    mutate(grouping = cumsum(table == "section"))
+    dplyr::select(line) %>%
+    tibble::add_column(table = "qa") %>%
+    dplyr::bind_rows(wh %>%
+                dplyr::select(line) %>%
+                tibble::add_column(table = "section")) %>%
+    dplyr::arrange(line) %>%
+    dplyr::mutate(grouping = cumsum(table == "section"))
 
   wh <- wh %>%
-    left_join(m_ind %>%
-                filter(table == "section")) %>%
-    select(-c(code, line, table))
+    dplyr::left_join(m_ind %>%
+                dplyr::filter(table == "section"), by = "line") %>%
+    dplyr::select(-c(code, line, table))
 
   qa_lines <- qa_lines %>%
-    left_join(m_ind %>%
-                filter(table == "qa")) %>%
-    select(-c(table))
+    dplyr::left_join(m_ind %>%
+                dplyr::filter(table == "qa"), by = "line") %>%
+    dplyr::select(-c(table))
 
   parsed_qa <- wh %>%
-    left_join(qa_lines, by = "grouping") %>%
-    select(-grouping)
+    dplyr::left_join(qa_lines, by = "grouping") %>%
+    dplyr::select(-grouping)
 
   return(parsed_qa)
 
@@ -150,28 +149,28 @@ qa_parse <- function(filepath) {
 
 qa_file <- function(filepath) { #TODO add status messages as to what is happening
 
-  code_file <- path_file(filepath)
+  code_file <- fs::path_file(filepath)
 
-  code_path <- path_dir(filepath)
+  code_path <- fs::path_dir(filepath)
 
   project_path <- rstudioapi::getActiveProject()
 
-  project_name <- str_extract(project_path, "(?!(.*\\/)).*")
+  project_name <- stringr::str_extract(project_path, "(?!(.*\\/)).*")
 
   if(code_path == project_path) { #If we're working with a script in the project root, it gets a QA sheet with the same name as the project
 
-    qa_file <- path(code_path, "QA", paste0("QA_", project_name, ".xlsx"))
+    qa_file <- fs::path(code_path, "QA", paste0("QA_", project_name, ".xlsx"))
 
-    fs::dir_create(path_dir(qa_file)) #function ignores command if dir already exists
+    fs::dir_create(fs::path_dir(qa_file)) #function ignores command if dir already exists
 
   } else if(str_detect(code_path, project_path)) { #If the script is in a subdir of project root, it gets a QA sheet with the name of the subdir.
 
     code_subfolder <- str_remove(code_path, project_path) %>%
       str_remove("/")
 
-    qa_file <- path(code_path, "QA", paste0("QA_", code_subfolder, ".xlsx"))
+    qa_file <- fs::path(code_path, "QA", paste0("QA_", code_subfolder, ".xlsx"))
 
-    fs::dir_create(path_dir(qa_file)) #function ignores command if dir already exists
+    fs::dir_create(fs::path_dir(qa_file)) #function ignores command if dir already exists
   }
 
   return(qa_file)
@@ -179,12 +178,13 @@ qa_file <- function(filepath) { #TODO add status messages as to what is happenin
 
 qa_wb <- function(filepath, qa_file) {
 
+  code_file <- fs::path_file(filepath)
 
   has_existing_qa_file <- fs::file_exists(qa_file)
 
   if(has_existing_qa_file) {
 
-    if(code_file %in% getSheetNames(qa_file)) {
+    if(code_file %in% openxlsx::getSheetNames(qa_file)) {
 
       cli::cli_alert_warning("A QA file and code review worksheet for this script already exists. If you continue, the worksheet code review section will be over-written (checklist and other sheets will not be affected).") #TODO: Check if there is any user-entered changes that will be deleted, and either make sure they aren't by lining up the QA tags, or prompt the user about this.  For now we are prompting every time regarding overwrite.
 
@@ -192,14 +192,14 @@ qa_wb <- function(filepath, qa_file) {
 
       if(user_overwrite) {
 
-        qawb <- loadWorkbook(qa_file)
+        qawb <- openxlsx::loadWorkbook(qa_file)
 
-        backup_sheet <- paste(code_file, lubridate::now()) %>% str_remove_all("-|:") #TODO: Need to limit chars to 31, so need better naming
+        backup_sheet <- paste(code_file, lubridate::now()) %>% stringr::str_remove_all("-|:") #TODO: Need to limit chars to 31, so need better naming
         backup_sheet <- abbreviate(backup_sheet, 31) #FIXME temporary until above is fixed!
 
-        cloneWorksheet(qawb, backup_sheet, clonedSheet = code_file)
+        openxlsx::cloneWorksheet(qawb, backup_sheet, clonedSheet = code_file)
 
-        sheetVisibility(qawb)[sheetNamesIndex(qawb, backup_sheet)] <- "hidden"
+        openxlsx::sheetVisibility(qawb)[sheetNamesIndex(qawb, backup_sheet)] <- "hidden"
 
 
         } else stop("User exited.") #TODO Do something better here
@@ -207,20 +207,20 @@ qa_wb <- function(filepath, qa_file) {
     } else { #Sheet does not exist but QA file does
       cli::cli_alert_info('A QA file for for the scripts in this directory already exists, but a worksheet for this script does not. It will be added as a new spreadsheet (named "{maybe_qa_sheet}") in the file: {qa_file}')
 
-      qawb <- loadWorkbook(qa_file)
+      qawb <- openxlsx::loadWorkbook(qa_file)
 
-      cloneWorksheet(qawb, code_file, clonedSheet = "Code_Review_Template")
-      sheetVisibility(qawb)[sheetNamesIndex(qawb, code_file)] <- "visible"
+      openxlsx::cloneWorksheet(qawb, code_file, clonedSheet = "Code_Review_Template")
+      openxlsx::sheetVisibility(qawb)[sheetNamesIndex(qawb, code_file)] <- "visible"
     }
 
   } else { #No QA sheet exists yet
 
     cli::cli_alert_info("A QA file for this script was not detected. A new one will be created: {qa_file}, and a worksheet for the script {code_file} will be added.")
 
-    qawb <- loadWorkbook(fs::path_package("integral", "extdata/QA_Template_Coded_Analysis.xlsx"))
+    qawb <- openxlsx::loadWorkbook(fs::path_package("integral", "extdata/QA_Template_Coded_Analysis.xlsx"))
 
-    cloneWorksheet(qawb, code_file, clonedSheet = "Code_Review_Template")
-    sheetVisibility(qawb)[sheetNamesIndex(qawb, code_file)] <- "visible"
+    openxlsx::cloneWorksheet(qawb, code_file, clonedSheet = "Code_Review_Template")
+    openxlsx::sheetVisibility(qawb)[sheetNamesIndex(qawb, code_file)] <- "visible"
 
   }
 
@@ -231,31 +231,31 @@ qa_wb <- function(filepath, qa_file) {
 #Internal function to translate sheet name to sheet index for openxlsx functions that don't accept names.
 sheetNamesIndex <- function(qawb, lookup) {
 
-  name_ind <- enframe(names(qawb), name = "index", value = "name")
+  name_ind <- tibble::enframe(names(qawb), name = "index", value = "name")
 
   if(is.character(lookup)) {
     name_ind %>%
-      filter(name == !!lookup) %>%
-      pull(index)
+      dplyr::filter(name == !!lookup) %>%
+      dplyr::pull(index)
   } else
   if(is.numeric(lookup)) {
     name_ind %>%
-      filter(index == name_ind) %>%
-      pull(name)
+      dplyr::filter(index == name_ind) %>%
+      dplyr::pull(name)
   } else stop("Sheet name or index number does not exist in workbook.")
 }
 
 
 
-qa_update_sheet <- function(qawb, parsed_qa, filepath) {
+qa_update_sheet <- function(qawb, parsed_qa, filepath, qa_file) {
 
-  code_file <- path_file(filepath)
+  code_file <- fs::path_file(filepath)
 
   qa_file <- qa_file(filepath)
 
-  writeData(qawb, code_file, parsed_qa, startRow = 14, colNames = F)
+  openxlsx::writeData(qawb, code_file, parsed_qa, startRow = 14, colNames = F)
 
-  saveWorkbook(qawb, )
+  openxlsx::saveWorkbook(qawb, qa_file)
 }
 
 #
@@ -281,6 +281,7 @@ qa_update_sheet <- function(qawb, parsed_qa, filepath) {
 
 
 
+#prefixer::prefixer()
 
 
 
