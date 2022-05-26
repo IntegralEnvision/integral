632e8d9edcaac0f0e43893d4058ce12592b8c478
Eben Pendleton <080051+ebenp@users.noreply.github.com>
2022-05-26 11:07:07 -0400

    Added vector handling

--- R/utils-columns.R
+++ R/utils-columns.R
@@ -1,7 +1,7 @@
 #' @title convert letter to column number
 #' @name letnum
 #' @description Converts an upper / lowercase letter into the resulting column
-#' number. Starts at 1.
+#' number. Starts at 1. Limited to current Excel maximum of XFD (16,384 columns)
 #' `r lifecycle::badge('stable')`
 #' @return Column number as integer
 #'
@@ -10,11 +10,36 @@
 #' @examples
 #' letnum("A")
 #'
+#' History
+#' 2022-05 Eben Pendleton written.
+
 letnum <- function(x) {
 
-  # Current Max column reference is XFD
-  if (any(nchar(x) > 1, length(x) > 1, x == "")) {
-    return(cli::cli_alert_danger("Error in letnum(): values must be single letters"))
+  # Current max column reference is XFD
+  maxcol <- "XFD"
+  # Generate 2 letter combinations
+  l2 <-  sort(c(outer(LETTERS,LETTERS,FUN=paste0)))
+
+  # Generate 3 combinations up to the current Excel max
+  l3 <- sort(c(outer(c(outer(LETTERS,LETTERS,FUN=paste0)), LETTERS,FUN=paste0)))
+  l3 <- l3[1:which(l3 %in% maxcol)]
+  # combine all possible letter combination
+  lets <- c(LETTERS, l2, l3)
+
+  each_let <-function(x) {
+    # see if we match a letter
+    result <- which(lets %in% toupper(x))
+    # if there is no letter match integer(0) is returned. Check and provide an
+    # error
+    if (identical(result, integer(0))) {
+      cli::cli_alert_danger(paste0("Error in letnum(): value not known: ",x
+                                   , "\nNA used in replacement"))
+      return(NA)
+    }
+    # explicitly return our result
+    return(result)
   }
-  which(LETTERS %in% toupper(x))
+  # try and return a number match for each letter
+  return(sapply(x, each_let, USE.NAMES = F))
+
 }
