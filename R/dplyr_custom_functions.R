b9d9aeacc706a3d71ea8ba1ebbae57cfd7215ddd
Jonathan Zadra <jzadra@integral-corp.com>
2024-03-14 10:42:20 -0600

    Add ordered factor case_match? not sure

--- R/dplyr_custom_functions.R
+++ R/dplyr_custom_functions.R
@@ -16,7 +16,7 @@ tp <- function(.data = .Last.value, rows = 10) {
 #' Ordered Factor case_when()
 #' @description
 #' `r lifecycle::badge('experimental')`
-#' Can replace `case_when()` syntax and outputs an ordered factor in the same order as the cases, useful for meaningful ordering in plots and tables.  This is because for `case_when()` the arguments are evaluated in order, so you must proceed from the most specific to the most general. Tables and plots will therefor be ordered by the evaluation order.
+#' Can replace `case_when()` syntax to output an ordered factor in the same order as the cases, useful for meaningful ordering in plots and tables.  This is because for `case_when()` the arguments are evaluated in order, so you must proceed from the most specific to the most general. Tables and plots will therefor be ordered by the evaluation order.
 #' @param ... A sequence of two-sided formulas. See ?dplyr::case_when for details
 #' @return An ordered factor vector of length 1 or n, matching the length of the logical input or output vectors, with the type (and attributes) of the first RHS. Inconsistent lengths or types will generate an error.
 #' @importFrom magrittr "%>%"
@@ -27,6 +27,22 @@ fct_case_when <- function(...) {
   levels <- levels[!is.na(levels)]
   ordered(dplyr::case_when(...), levels = levels)
 }
+
+#' Ordered Factor case_match()
+#' @description
+#' `r lifecycle::badge('experimental')`
+#' Can replace `case_match()` syntax to output an ordered factor in the same order as the cases, useful for meaningful ordering in plots and tables.  This is because for `case_match()` the arguments are evaluated in order, so you must proceed from the most specific to the most general. Tables and plots will therefor be ordered by the evaluation order.
+#' @param .x 	A vector to match against.
+#' @param ... <dynamic-dots> A sequence of two-sided formulas: old_values ~ new_value. The right hand side (RHS) determines the output value for all values of .x that match the left hand side (LHS). See ?case_when for details.
+#' The LHS must evaluate to the same type of vector as .x. It can be any length, allowing you to map multiple .x values to the same RHS value. If a value is repeated in the LHS, i.e. a value in .x matches to multiple cases, the first match is used.
+#' The RHS inputs will be coerced to their common type. Each RHS input will be recycled to the size of .x.
+#' @param .default The value used when values in .x aren't matched by any of the LHS inputs. If NULL, the default, a missing value will be used.
+#' .default is recycled to the size of .x.
+#' @param .ptype An optional prototype declaring the desired output type. If not supplied, the output type will be taken from the common type of all RHS inputs and .default.
+#' @return An ordered factor vector with the same size as .x
+#' @importFrom magrittr "%>%"
+#' @export
+
 
 #' Remove variables from tibble
 #' @description
