99cc528722751f807e97d86ebacf17aa6751437d
Jonathan Zadra <jzadra@integral-corp.com>
2024-04-10 17:19:51 -0600

    Fixed issue with no arguments provided; upgraded to stable badge.

--- R/shortcuts.R
+++ R/shortcuts.R
@@ -1,30 +1,31 @@
 #' Open cheatsheet
 #' @description
-#' `r lifecycle::badge('experimental')`
+#' `r lifecycle::badge('stable')`
 #' Shortcut to open a cheatsheet
-#' @param sheet Bare, unquoted name of the cheat sheet, if known. Partial matches work as well. If NULL (default) a list is presented.
+#' @param sheet Bare, unquoted name of the cheat sheet, if known. Partial matches work as well. If no argument is provided a list is presented.
 #' @export
 cheat <- function(sheet) {
-  sheet <- rlang::ensym(sheet) %>% rlang::as_string()
 
   files <- dir(system.file("extdata/cheatsheets", package = "integral")) %>%
     tools::file_path_sans_ext()
 
   if (!methods::hasArg(sheet)) { # No sheet specified; full listing
-    cli::cli_alert_info("Please select cheatsheet:")
+    cli::cli_alert_info("Please select cheatsheet or enter `0` to exit:")
     sheet <- utils::select.list(choices = files)
-  } else if (any(stringr::str_detect(files, sheet))) { # Matches
-    matches <- files[stringr::str_detect(files, sheet)]
-    if (length(matches) == 1) {
+  } else {
+    sheet <- rlang::ensym(sheet) %>% rlang::as_string()
+    if (any(stringr::str_detect(files, sheet))) { # Matches
+      matches <- files[stringr::str_detect(files, sheet)]
+      if (length(matches) == 1) {
       sheet <- matches
     } else {
-      cli::cli_alert_info("Multiple matching cheatsheets, select one:")
+      cli::cli_alert_info("Multiple matching cheatsheets, select one or enter `0` to exit:")
       sheet <- utils::select.list(choices = matches)
     }
-  } else {
-    cli::cli_alert_warning("No matching cheatsheets. Select from the list below:")
+  } else { #/Matches
+    cli::cli_alert_warning("No matching cheatsheets. Select from the list below or enter `0` to exit:")
     sheet <- utils::select.list(choices = files)
-  }
+  }}
 
   if(sheet == "") return(cli::cli_alert_info("User exited."))
 
